e04a1c3683925075f8a08b542f67f70199ee497b
Jaffa <affa@ecoph.tk>
2019-07-28 17:57:15 +0800

    1、设置裁判说App登录界面全盘显示 2、改变APP全局样式 3、裁判说登录界面布局 4、裁判说登录内容框背景shape

--- app/src/main/java/com/judge/views/LoginView.kt
+++ app/src/main/java/com/judge/views/LoginView.kt
@@ -2,6 +2,7 @@ package com.judge.views
 
 import android.content.Context
 import android.util.AttributeSet
+import android.widget.FrameLayout
 import android.widget.LinearLayout
 import com.airbnb.epoxy.ModelView
 import com.judge.R
@@ -10,12 +11,12 @@ import com.judge.R
  * @author: jaffa
  * @date: 2019/7/28
  */
-@ModelView(autoLayout = ModelView.Size.MATCH_WIDTH_WRAP_HEIGHT)
+@ModelView(autoLayout = ModelView.Size.MATCH_WIDTH_MATCH_HEIGHT)
 class LoginView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : LinearLayout(context, attrs, defStyleAttr) {
+) : FrameLayout(context, attrs, defStyleAttr) {
     init {
         inflate(context, R.layout.view_login, this)
     }
