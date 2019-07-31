86459a048edfd5cb8bbf6412613175d72ccc5c61
sumile <sumile@ecoph.tk>
2019-07-31 09:51:38 +0800

    我的页面列表项 实现

--- app/src/main/java/com/judge/adapters/BindingAdapters.kt
+++ app/src/main/java/com/judge/adapters/BindingAdapters.kt
@@ -1,12 +1,25 @@
 package com.judge.adapters
 
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.judge.data.MineItem
 import com.squareup.picasso.Picasso
+import com.vondear.rxtool.RxTool
 
 @BindingAdapter("imageUrl", requireAll = false)
 fun setImage(imageView: ImageView, imageUrl: String) {
     Picasso.with(imageView.context).load(imageUrl)
         .into(imageView)
+}
+
+@BindingAdapter("drawableSrc", requireAll = false)
+fun setDrawable(textView: TextView, item: MineItem) {
+    textView.setCompoundDrawablesWithIntrinsicBounds(
+        RxTool.getContext().resources.getDrawable(item.leftIconIdRes),
+        null,
+        RxTool.getContext().resources.getDrawable(item.rightIconIdRes),
+        null
+    )
 }
 
--- app/src/main/java/com/judge/package-info.java
+++ app/src/main/java/com/judge/package-info.java
@@ -1,6 +1,6 @@
 //@EpoxyDataBindingPattern(rClass = R.class, layoutPrefix = "epoxy_layout")
 
-@EpoxyDataBindingLayouts({R.layout.dog_row_binding, R.layout.video_row})
+@EpoxyDataBindingLayouts({R.layout.dog_row_binding, R.layout.video_row, R.layout.mine_item})
 package com.judge;
 
 import com.airbnb.epoxy.EpoxyDataBindingLayouts;
