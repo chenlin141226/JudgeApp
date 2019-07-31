c3903b98a3ccdba16d81e7957694f28724886f00
sumile <sumile@ecoph.tk>
2019-07-31 16:27:01 +0800

    我的页面编写

---
+++ app/src/main/java/com/judge/data/User.kt
@@ -0,0 +1,9 @@
+package com.judge.data
+
+data class User(
+    val userName: String,
+    val password: String,
+    val level: String,
+    val userId: Int,
+    val userPhoto: String
+)
--- app/src/main/java/com/judge/package-info.java
+++ app/src/main/java/com/judge/package-info.java
@@ -1,6 +1,8 @@
 //@EpoxyDataBindingPattern(rClass = R.class, layoutPrefix = "epoxy_layout")
 
-@EpoxyDataBindingLayouts({R.layout.dog_row_binding, R.layout.video_row, R.layout.mine_item})
+@EpoxyDataBindingLayouts({R.layout.dog_row_binding,
+        R.layout.video_row, R.layout.mine_item
+        , R.layout.mine_title})
 package com.judge;
 
 import com.airbnb.epoxy.EpoxyDataBindingLayouts;
