4d14613fba56dc526a87ec2aa14b2abb8ebc6ce0
sumile <sumile@ecoph.tk>
2019-08-05 13:32:00 +0800

    添加主题界面

--- app/src/main/java/com/judge/package-info.java
+++ app/src/main/java/com/judge/package-info.java
@@ -1,19 +1,14 @@
 //@EpoxyDataBindingPattern(rClass = R.class, layoutPrefix = "epoxy_layout")
 
-import com.airbnb.epoxy.EpoxyDataBindingLayouts;
-import com.judge.R;
-
 @EpoxyDataBindingLayouts({R.layout.dog_row_binding,
         R.layout.video_row, R.layout.mine_item,
         R.layout.mine_title,
         R.layout.whistle_item,
         R.layout.whistle_rules_item,
         R.layout.message_item,
+        R.layout.market_item,
         R.layout.personal_message_item,
-        R.layout.public_message_item,
-        R.layout.market_item})
-        R.layout.public_message_item,
-        R.layout.topic_item})
+        R.layout.public_message_item})
 package com.judge;
 
 import com.airbnb.epoxy.EpoxyDataBindingLayouts;
