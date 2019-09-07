16e858a4e777bae83a37b7af4859239e93a74c18
Jaffa <affa@ecoph.tk>
2019-09-07 11:11:12 +0800

    发帖关注板块，推荐板块接口对接，逻辑处理，数据结构处理， 界面数据传递，事件处理

--- app/src/main/java/com/judge/app/fragments/topic/ContributeplateFragment.kt
+++ app/src/main/java/com/judge/app/fragments/topic/ContributeplateFragment.kt
@@ -1,24 +1,78 @@
 package com.judge.app.fragments.topic
 
+import android.os.Parcelable
 import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
 import com.airbnb.mvrx.MvRxState
 import com.airbnb.mvrx.MvRxViewModelFactory
 import com.airbnb.mvrx.ViewModelContext
+import com.airbnb.mvrx.fragmentViewModel
+import com.jeremyliao.liveeventbus.LiveEventBus
 import com.judge.R
 import com.judge.app.core.BaseFragment
 import com.judge.app.core.MvRxViewModel
 import com.judge.app.core.simpleController
+import com.judge.data.bean.PlateBean
+import com.judge.data.bean.PlateCategory
+import com.judge.data.repository.JudgeRepository
+import com.judge.extensions.clear
+import com.judge.plateContentItem
 import com.judge.plateItem
+import com.judge.utils.LogUtils
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.parcel.Parcelize
+import org.jetbrains.anko.collections.forEachWithIndex
 
 /**
  * @author: jaffa
  * @date: 2019/9/6
  */
+@Parcelize
+data class PlateArgs @JvmOverloads constructor(
+    var plateName: String = "",
+    var formhash: String = ""
+) : Parcelable
+
 data class PlateState(
-    val isLoading: Boolean = false
+    val isLoading: Boolean = false,
+    val plate: List<PlateCategory> = emptyList(),
+    val attentionPlate: List<PlateCategory> = emptyList(),
+    val recommendPlate: List<PlateCategory> = emptyList(),
+    val variables: PlateBean? = null
 ) : MvRxState
 
 class PlateViewModel(initialiState: PlateState) : MvRxViewModel<PlateState>(initialiState) {
+
+    fun fetchPlate() {
+        JudgeRepository.getPlate().subscribeOn(Schedulers.io())
+            .doOnSubscribe { setState { copy(isLoading = true) } }
+            .doOnError { it.message.let { it1 -> LogUtils.e(it1!!) } }
+            .doFinally { setState { copy(isLoading = false) } }
+            .execute {
+                copy(
+                    variables = it()?.Variables,
+                    plate = it()?.Variables?.forumlist ?: emptyList(),
+                    attentionPlate = it()?.Variables?.forumlist?.filterIndexed { _, plateCategory ->
+                        plateCategory.favorite == "1"
+                    } ?: emptyList(),
+                    recommendPlate = it()?.Variables?.forumlist?.filterIndexed { _, plateCategory ->
+                        plateCategory.favorite == "0"
+                    } ?: emptyList()
+                )
+            }
+    }
+
+    //重置
+    fun clearPlate() {
+        setState {
+            copy(
+                plate = plate.clear(),
+                attentionPlate = attentionPlate.clear(),
+                recommendPlate = recommendPlate.clear()
+            )
+        }
+    }
+
     companion object : MvRxViewModelFactory<PlateViewModel, PlateState> {
         override fun create(
             viewModelContext: ViewModelContext,
@@ -31,15 +85,37 @@ class PlateViewModel(initialiState: PlateState) : MvRxViewModel<PlateState>(init
 
 class ContributeplateFragment : BaseFragment() {
 
-    override fun epoxyController() = simpleController {
+    val viewModel: PlateViewModel by fragmentViewModel()
+    val plateArgs = PlateArgs()
+    override fun epoxyController() = simpleController(viewModel) { state ->
         plateItem {
             id("attentions")
             title(resources.getString(R.string.attention_plate))
         }
 
+        state.attentionPlate.forEachWithIndex { _, item ->
+            plateContentItem {
+                id(state.plate.size)
+                item(item)
+                onclick { _ ->
+                    plateArgs.plateName = item.name
+                    plateArgs.formhash = state.variables?.formhash.toString()
+                    LiveEventBus.get().with("plate").post(plateArgs)
+                    findNavController().navigateUp()
+                }
+            }
+        }
+
         plateItem {
             id("recommend")
             title(resources.getString(R.string.recommend_plate))
+        }
+
+        state.recommendPlate.forEachWithIndex { _, item ->
+            plateContentItem {
+                id(state.plate.size)
+                item(item)
+            }
         }
     }
 
@@ -47,5 +123,38 @@ class ContributeplateFragment : BaseFragment() {
         super.initView()
         toolbar.isVisible = true
         toolbar.title = "投稿模板"
+
+
+        viewModel.selectSubscribe(PlateState::isLoading) {
+            if (it) {
+                loadingDialog.show()
+            } else {
+                loadingDialog.dismiss()
+            }
+        }
+        refreshLayout.apply {
+            setEnableAutoLoadMore(true)
+            setEnableRefresh(true)
+            setEnableLoadMore(true)
+            setOnRefreshListener {
+                viewModel.fetchPlate()
+                it.finishRefresh(1000)
+            }
+
+            setOnLoadMoreListener {
+                it.finishLoadMore(1000)
+            }
+        }
+
+    }
+
+    override fun initData() {
+        super.initData()
+        viewModel.fetchPlate()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        viewModel.clearPlate()
     }
 }
--- app/src/main/java/com/judge/app/fragments/topic/SignFragment.kt
+++ app/src/main/java/com/judge/app/fragments/topic/SignFragment.kt
@@ -3,11 +3,13 @@ package com.judge.app.fragments.topic
 import android.text.InputType
 import android.view.View
 import androidx.core.view.isVisible
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.airbnb.mvrx.MvRxState
 import com.airbnb.mvrx.MvRxViewModelFactory
 import com.airbnb.mvrx.ViewModelContext
 import com.airbnb.mvrx.fragmentViewModel
+import com.jeremyliao.liveeventbus.LiveEventBus
 import com.judge.R
 import com.judge.app.core.BaseFragment
 import com.judge.app.core.MvRxViewModel
@@ -21,22 +23,33 @@ import org.jetbrains.anko.sdk27.coroutines.onClick
  * @date: 2019/8/22
  */
 data class SendTopicState(
-    val title : String? = "",
-    val content : String? = "",
-    val plate : String? = ""
-):MvRxState
+    val title: String? = "",
+    val content: String? = "",
+    val plate: String? = "",
+    val backPlate: String? = "",
+    val formhash: String = ""
+) : MvRxState
 
-class SendTopicViewModel(initialState: SendTopicState) : MvRxViewModel<SendTopicState>(initialState) {
+class SendTopicViewModel(initialState: SendTopicState) :
+    MvRxViewModel<SendTopicState>(initialState) {
 
-    fun updateTitlet(title :String){
+    fun updateArgs(args: PlateArgs) {
+        setState { copy(backPlate = args.plateName,formhash = args.formhash)}
+    }
+
+    fun updateTitlet(title: String) {
         setState { copy(title = title) }
     }
 
-    fun updateContent(content :String){
+    fun updateContent(content: String) {
         setState { copy(content = content) }
     }
 
-    companion object : MvRxViewModelFactory<SendTopicViewModel,SendTopicState>{
+    fun pushTie() = withState { state ->
+
+    }
+
+    companion object : MvRxViewModelFactory<SendTopicViewModel, SendTopicState> {
         override fun create(
             viewModelContext: ViewModelContext,
             state: SendTopicState
@@ -49,22 +62,22 @@ class SendTopicViewModel(initialState: SendTopicState) : MvRxViewModel<SendTopic
 
 class SignFragment : BaseFragment() {
 
-    val viewModel : SendTopicViewModel by fragmentViewModel()
+    val viewModel: SendTopicViewModel by fragmentViewModel()
 
-    override fun epoxyController() = simpleController(viewModel) {state ->
-           sendtopicItem {
-               id("sendtopic")
-               inputType(InputType.TYPE_CLASS_TEXT)
-               title("")
-               content("")
-               titleTextWatcher(SimpleTextWatcher{viewModel.updateTitlet(it)})
+    override fun epoxyController() = simpleController(viewModel) { state ->
+        sendtopicItem {
+            id("sendtopic")
+            inputType(InputType.TYPE_CLASS_TEXT)
+            title(state.title)
+            content(state.content)
+            titleTextWatcher(SimpleTextWatcher { viewModel.updateTitlet(it) })
 
-               contentTextWatcher(SimpleTextWatcher{viewModel.updateContent(it)})
+            contentTextWatcher(SimpleTextWatcher { viewModel.updateContent(it) })
 
-               onclick { _ ->
-                   findNavController().navigate(R.id.action_signFragment_to_contributeplate)
-               }
-           }
+            onclick { _ ->
+                findNavController().navigate(R.id.action_signFragment_to_contributeplate)
+            }
+        }
     }
 
     override fun initView() {
@@ -74,9 +87,16 @@ class SignFragment : BaseFragment() {
             text = resources.getString(R.string.publish)
             visibility = View.VISIBLE
             onClick {
-
+                viewModel.pushTie()
             }
         }
+    }
+
+    override fun initData() {
+        super.initData()
+        LiveEventBus.get().with("plate", PlateArgs::class.java).observe(this, Observer<PlateArgs> {
+            viewModel.updateArgs(it)
+        })
     }
 
     override fun onDestroyView() {
