adc063ecb8141bae63c13748546d3b6839880a39
sumile <sumile@ecoph.tk>
2019-09-17 16:14:10 +0800

    资讯详情页 toolbar menu设置，点击事件等处理

--- app/src/main/java/com/judge/app/fragments/home/NewsDetailFragment.kt
+++ app/src/main/java/com/judge/app/fragments/home/NewsDetailFragment.kt
@@ -1,15 +1,33 @@
 package com.judge.app.fragments.home
 
+import androidx.core.view.isVisible
+import com.judge.R
 import com.judge.app.core.BaseFragment
 import com.judge.app.core.MvRxEpoxyController
 import com.judge.app.core.simpleController
+import org.jetbrains.anko.appcompat.v7.coroutines.onMenuItemClick
+import org.jetbrains.anko.support.v4.toast
 
-class NewsDetailFragment :BaseFragment(){
-    override fun epoxyController(): MvRxEpoxyController=simpleController {
+class NewsDetailFragment : BaseFragment() {
+    override fun epoxyController(): MvRxEpoxyController = simpleController {
     }
 
     override fun initView() {
         super.initView()
-
+        setHasOptionsMenu(true)
+        toolbar.apply {
+            inflateMenu(R.menu.menu_main)
+            isVisible = true
+            onMenuItemClick { item ->
+                when (item?.itemId) {
+                    R.id.action_share -> {
+                        toast("share")
+                    }
+                    else -> {
+                    }
+                }
+            }
+        }
     }
+
 }
